5bb852041f17c2b9b9abd987315060626a40ea88
Majid-Arab <arabmajid12@gmail.com>
2025-01-26 22:37:55 +0500

    created navbar

---
+++ components/animata/container/nav-tabs.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+import { cn } from "../../../libs/utils";
+import Image from "next/image";
+
+interface TabProps {
+  text: string;
+  selected: boolean;
+  setSelected: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function NavTabs({ tabs }: { tabs: string[] }) {
+  const [selected, setSelected] = useState<string>(tabs[0]);
+
+  return (
+    <div className="flex flex-wrap items-center justify-around gap-4 p-2">
+      <Image src="/logo.png" alt="Logo" width={100} height={100} />
+      <div>
+        {tabs.map((tab) => (
+          <Tab text={tab} selected={selected === tab} setSelected={setSelected} key={tab} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const Tab = ({ text, selected, setSelected }: TabProps) => {
+  return (
+    <button
+      onClick={() => setSelected(text)}
+      className={cn(
+        "relative rounded-md p-2 text-sm transition-all",
+        selected ? "text-white" : "text-slate-300 hover:font-black",
+      )}
+    >
+      <p className="relative z-50 min-w-20">{text}</p>
+      {selected && (
+        <motion.span
+          layoutId="tabs"
+          transition={{ type: "spring", duration: 0.5 }}
+          className="absolute inset-0 rounded-sm bg-gradient-to-r from-cyan-700 to-red-400"
+        />
+      )}
+    </button>
+  );
+};
