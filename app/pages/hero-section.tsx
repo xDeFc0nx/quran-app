60470659d845b8a057e8cccfbd4f8663e16b6210
Abdul-Majid <arabmajid12@gmail.com>
2025-01-27 14:58:43 +0500

    setup beam lights in hero section

--- app/pages/hero-section.tsx
+++ app/pages/hero-section.tsx
@@ -1,32 +1,26 @@
 "use client";
-
-import { AuroraBackground } from "@/components/aceternity/ui/aurora-background";
-import { motion } from "framer-motion";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 import React from "react";
 
 export function HeroSection() {
   return (
-    <AuroraBackground>
-      <motion.div
-        initial={{ opacity: 0.0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="relative flex flex-col gap-4 items-center justify-center px-4"
-      >
-        <div className="text-3xl md:text-7xl font-bold dark:text-white text-center">
-          Background lights are cool you know.
-        </div>
-        <div className="font-extralight text-base md:text-4xl dark:text-neutral-200 py-4">
-          And this, is chemical burn.
-        </div>
-        <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
-          Debug now
-        </button>
-      </motion.div>
-    </AuroraBackground>
+    <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-6xl mx-auto p-4">
+        <h1 className="relative z-10 text-lg md:text-5xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+          At Sevteen, we fuse modern technology with timeless Islamic values,
+          helping the Ummah stay seamlessly connected to their Deen wherever
+          they are
+        </h1>
+        <p></p>
+        {/* <p className="text-neutral-500 max-w-4xl mx-auto my-2 text-lg text-center relative z-10">
+          Welcome to MailJet, the best transactional email service on the web.
+          We provide reliable, scalable, and customizable email solutions for
+          your business. Whether you&apos;re sending order confirmations,
+          password reset emails, or promotional campaigns, MailJet has got you
+          covered.
+        </p> */}
+      </div>
+      <BackgroundBeams />
+    </div>
   );
 }
