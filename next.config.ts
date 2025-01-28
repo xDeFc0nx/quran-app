891d65da509d29c58e53a033643588965a6cb5db
Abdul-Majid <arabmajid12@gmail.com>
2025-01-28 10:48:57 +0500

    created carousel page and linked with home page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "via.placeholder.com", // Allow placeholder images
+      "images.unsplash.com", // Allow Unsplash images
+    ],
+    formats: ["image/avif", "image/webp"], // Enable modern image formats for optimization
+  },
+  env: {
+    UNSPLASH_ACCESS_KEY: "J1fVSClatIlHRo-UUQUm6CCWrF9Rd16sNnwW4yL6tiA", // Access key for Unsplash API
+  },
 };
 
 export default nextConfig;
