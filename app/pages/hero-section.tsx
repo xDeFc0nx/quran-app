56a589263a4f185bddb5fd8d1706ad1064a05a32
Majid-Arab <arabmajid12@gmail.com>
2025-01-28 21:24:53 +0500

    hero section height

--- app/pages/hero-section.tsx
+++ app/pages/hero-section.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 export function HeroSection() {
   return (
-    <div className="h-[800px] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+    <div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-6xl m-auto p-4 flex flex-col items-center gap-10">
         <TextGenerator />
         <Link
