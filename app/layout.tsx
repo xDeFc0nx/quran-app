dea7f619ac6efffc82fdfa780eb5e7e91b251e68
Abdul-Majid <arabmajid12@gmail.com>
2025-01-28 14:33:58 +0500

    fixed navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
           <NavTabs
             tabs={[
               'Home',
-              'FEATURES',
+              'Features',
               'Collection',
               'Qibla',
               'Settings']}
--- components/animata/container/nav-tabs.tsx
+++ components/animata/container/nav-tabs.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useState } from "react";
 import { motion } from "framer-motion";
 import { cn } from "../../../lib/utils";
@@ -8,20 +9,23 @@ import Link from "next/link";
 interface TabProps {
   text: string;
   selected: boolean;
+  href: string;
   setSelected: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export default function NavTabs({ tabs }: { tabs: string[] }) {
-  const [selected, setSelected] = useState<string>(tabs[0]);
+  const sanitizedTabs = tabs.filter((tab) => tab.trim().length > 0); // Filter out invalid tabs
+  const [selected, setSelected] = useState<string>(sanitizedTabs[0]);
 
   return (
     <div className="flex flex-wrap items-center justify-around gap-4 p-2">
       <Link href="/">
         <Image src="/logo.png" alt="Logo" width={100} height={100} />
       </Link>
-      <div>
-        {tabs.map((tab) => (
+      <div className="flex text-center">
+        {sanitizedTabs.map((tab) => (
           <Tab
+            href={tab}
             text={tab}
             selected={selected === tab}
             setSelected={setSelected}
@@ -33,14 +37,25 @@ export default function NavTabs({ tabs }: { tabs: string[] }) {
   );
 }
 
-const Tab = ({ text, selected, setSelected }: TabProps) => {
+const Tab = ({ text, selected, setSelected, href }: TabProps) => {
+  const url = (href: string): string => {
+    const normalizedHref = href?.trim().toLowerCase() || "";
+    if (!normalizedHref) {
+      console.warn("The href parameter is empty or undefined.");
+      return "/";
+    }
+    return normalizedHref === "home" ? "/" : `/${normalizedHref}`;
+  };
+
   return (
-    <button
+    <Link
+      href={url(href)}
       onClick={() => setSelected(text)}
       className={cn(
-        "relative rounded-md p-2 text-sm transition-all",
+        "relative flex rounded-md p-2 text-sm transition-all",
         selected ? "text-white" : "text-slate-300 hover:font-black"
       )}
+      aria-current={selected ? "page" : undefined}
     >
       <p className="relative z-50 min-w-20">{text}</p>
       {selected && (
@@ -50,6 +65,6 @@ const Tab = ({ text, selected, setSelected }: TabProps) => {
           className="absolute inset-0 rounded-sm bg-gradient-to-r from-logoGray to-logoBlue"
         />
       )}
-    </button>
+    </Link>
   );
 };
