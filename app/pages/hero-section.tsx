06fd11e8965c38306b03127d39b2b354ab3d79cd
Abdul-Majid <arabmajid12@gmail.com>
2025-01-28 11:34:20 +0500

    changed carousel to collection

--- app/pages/hero-section.tsx
+++ app/pages/hero-section.tsx
@@ -10,7 +10,7 @@ export function HeroSection() {
       <div className="max-w-6xl m-auto p-4 flex flex-col items-center gap-10">
         <TextGenerator />
         <Link
-          href="/carousel"
+          href="/collection"
           className="inline-flex h-12 z-10 animate-shimmer items-center justify-center rounded-md border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-xl text-slate-400 transition-colors hover:border-2 focus:outline-none focus:ring-0 focus:ring-slate-50 focus:ring-offset-2 focus:ring-offset-slate-50"
         >
           Get Started
--- components/animata/container/nav-tabs.tsx
+++ components/animata/container/nav-tabs.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { cn } from "../../../lib/utils";
 import Image from "next/image";
+import Link from "next/link";
 
 interface TabProps {
   text: string;
@@ -15,10 +16,17 @@ export default function NavTabs({ tabs }: { tabs: string[] }) {
 
   return (
     <div className="flex flex-wrap items-center justify-around gap-4 p-2">
-      <Image src="/logo.png" alt="Logo" width={100} height={100} />
+      <Link href="/">
+        <Image src="/logo.png" alt="Logo" width={100} height={100} />
+      </Link>
       <div>
         {tabs.map((tab) => (
-          <Tab text={tab} selected={selected === tab} setSelected={setSelected} key={tab} />
+          <Tab
+            text={tab}
+            selected={selected === tab}
+            setSelected={setSelected}
+            key={tab}
+          />
         ))}
       </div>
     </div>
@@ -31,7 +39,7 @@ const Tab = ({ text, selected, setSelected }: TabProps) => {
       onClick={() => setSelected(text)}
       className={cn(
         "relative rounded-md p-2 text-sm transition-all",
-        selected ? "text-white" : "text-slate-300 hover:font-black",
+        selected ? "text-white" : "text-slate-300 hover:font-black"
       )}
     >
       <p className="relative z-50 min-w-20">{text}</p>
