67910c585e037766ea586cf37b83370fb79cad5d
Abdul-Majid <arabmajid12@gmail.com>
2025-01-28 10:50:19 +0500

    removed h1 from hero section

--- app/pages/hero-section.tsx
+++ app/pages/hero-section.tsx
@@ -8,9 +8,7 @@ export function HeroSection() {
   return (
     <div className="h-[800px] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-6xl m-auto p-4 flex flex-col items-center gap-10">
-        <h1 className="relative z-10 text-lg md:text-5xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-          <TextGenerator />
-        </h1>
+        <TextGenerator />
         <Link
           href="/carousel"
           className="inline-flex h-12 z-10 animate-shimmer items-center justify-center rounded-md border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-xl text-slate-400 transition-colors hover:border-2 focus:outline-none focus:ring-0 focus:ring-slate-50 focus:ring-offset-2 focus:ring-offset-slate-50"
