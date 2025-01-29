1694de0fdc1d1aa234a600ee3c7d189c102c2a21
Abdul-Majid <arabmajid12@gmail.com>
2025-01-29 11:34:06 +0500

    deployment error fixes :/

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -1,21 +1,13 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-import { Card } from "@/components/ui/apple-cards-carousel";
 import { TextEffect } from "@/components/ui/text-effect";
 import { Sparkles } from "@/components/aceternity/sparkles";
 import MoodCards from "@/components/animata/card/shiny-card";
 
 export default function CardsCarousel() {
-  // const cards = data.map((card, index) => (
-  //   <Card key={card.src} card={card} index={index} />
-  // ));
-
   return (
     <div className="flex flex-col justify-center pt-40">
-      {/* <h2 className="max-w-7xl mx-auto text-xl md:text-5xl font-bold text-neutral-200 font-sans">
-        Choose your path
-      </h2> */}
       <Sparkles name="Choose your Mood" />
 
       <MoodCards />
@@ -55,51 +47,11 @@ const DummyContent = () => {
   );
 };
 
-const data = [
-  {
-    category: "Artificial Intelligence",
-    title: "You can do more with AI.",
-    src: "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?q=80&w=3556&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-  {
-    category: "Productivity",
-    title: "Enhance your productivity.",
-    src: "https://images.unsplash.com/photo-1531554694128-c4c6665f59c2?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-  {
-    category: "Product",
-    title: "Launching the new Apple Vision Pro.",
-    src: "https://images.unsplash.com/photo-1713869791518-a770879e60dc?q=80&w=2333&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-
-  {
-    category: "Product",
-    title: "Maps for your iPhone 15 Pro Max.",
-    src: "https://images.unsplash.com/photo-1599202860130-f600f4948364?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-  {
-    category: "iOS",
-    title: "Photography just got better.",
-    src: "https://images.unsplash.com/photo-1602081957921-9137a5d6eaee?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-  {
-    category: "Hiring",
-    title: "Hiring for a Staff Software Engineer",
-    src: "https://images.unsplash.com/photo-1511984804822-e16ba72f5848?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    content: <DummyContent />,
-  },
-];
-
 export function TextEffectPerChar() {
   return (
-    <TextEffect per='char' preset='fade' className="text-lg max-w-2xl ">
-      Generate Quranic ayahs tailored to your mood, discover
-      random verses for fresh wisdom, or find the Qibla in an instant.
+    <TextEffect per="char" preset="fade" className="text-lg max-w-2xl ">
+      Generate Quranic ayahs tailored to your mood, discover random verses for
+      fresh wisdom, or find the Qibla in an instant.
     </TextEffect>
   );
 }
