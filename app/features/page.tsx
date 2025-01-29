e1458fe3aa4cfd6a2d8693d4a9cc2a831552f5a8
Abdul-Majid <arabmajid12@gmail.com>
2025-01-29 11:39:44 +0500

    dp fix

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import React from "react";
 import { TextEffect } from "@/components/ui/text-effect";
 import { Sparkles } from "@/components/aceternity/sparkles";
