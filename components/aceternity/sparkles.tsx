3fc5e8b106e32bc5b55d1fcf60aa3bb6278dbdaa
Abdul-Majid <arabmajid12@gmail.com>
2025-01-29 10:18:15 +0500

    fixed sparkles underline styling

--- components/aceternity/sparkles.tsx
+++ components/aceternity/sparkles.tsx
@@ -4,11 +4,11 @@ import { SparklesCore } from "../ui/sparkles";
 
 export function Sparkles() {
     return (
-        <div className="h-[40rem] w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
-            <h1 className="md:text-7xl text-3xl lg:text-9xl font-bold text-center text-white relative z-20">
+        <div className="w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+            <h1 className="text-lg md:text-5xl font-bold text-center text-white relative z-20">
                 Choose your path
             </h1>
-            <div className="w-[40rem] h-40 relative">
+            <div className="w-[40rem] h-24 relative">
                 {/* Gradients */}
                 <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
                 <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
