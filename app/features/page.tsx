0c1ab857f27bfb7464a67ef755755ee9f4d80f40
Majid-Arab <arabmajid12@gmail.com>
2025-02-01 07:44:52 +0500

    setup features page styling for responsiveness

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -5,7 +5,7 @@ import MoodCards from "@/components/animata/card/shiny-card";
 
 export default function CardsCarousel() {
   return (
-    <div className="flex flex-col justify-center pt-40">
+    <div className="flex flex-col justify-center uppercase pt-10 pb-20 md:pt-40">
       <Sparkles name="Choose your Mood" />
 
       <MoodCards />
