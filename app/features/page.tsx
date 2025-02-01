4410abd26382b6773d1ffe5f8fbe8a9add462016
Majid-Arab <arabmajid12@gmail.com>
2025-02-01 13:10:20 +0500

    removed scoll from desktop features cards

--- app/features/page.tsx
+++ app/features/page.tsx
@@ -5,7 +5,7 @@ import MoodCards from "@/components/animata/card/shiny-card";
 
 export default function CardsCarousel() {
   return (
-    <div className="flex flex-col justify-center uppercase pt-10 pb-20 md:pt-40">
+    <div className="flex flex-col justify-center items-center uppercase pt-10 pb-20 md:pt-20 md:pb-0 md:h-screen">
       <Sparkles name="Choose your Mood" />
 
       <MoodCards />
